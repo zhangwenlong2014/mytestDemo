1ec0a6b484dafbd645a8d5b34361d223a48b020b
zhangwenlong <zhangwenlong_2013@126.com>
2017-02-22 15:53:03 +0800

    新建文件kbia

---
+++ testDemo/testDemo/KbidoController.m
@@ -0,0 +1,39 @@
+//
+//  KbidoController.m
+//  testDemo
+//
+//  Created by zhangwenlong on 2017/2/22.
+//  Copyright © 2017年 Demo. All rights reserved.
+//
+
+#import "KbidoController.h"
+
+@interface KbidoController ()
+
+@end
+
+@implementation KbidoController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+     self.view.backgroundColor = [UIColor redColor];
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
