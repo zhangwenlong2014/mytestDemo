491459c71544f211be6923c54ce97ed63239c655
zhangwenlong <zhangwenlong_2013@126.com>
2017-02-22 16:05:07 +0800

    修改bug添加

---
+++ testDemo/testDemo/AddBugaiewController.h
@@ -0,0 +1,13 @@
+//
+//  AddBugaiewController.h
+//  testDemo
+//
+//  Created by zhangwenlong on 2017/2/22.
+//  Copyright © 2017年 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddBugaiewController : UIViewController
+
+@end
---
+++ testDemo/testDemo/AddBugaiewController.m
@@ -0,0 +1,38 @@
+//
+//  AddBugaiewController.m
+//  testDemo
+//
+//  Created by zhangwenlong on 2017/2/22.
+//  Copyright © 2017年 Demo. All rights reserved.
+//
+
+#import "AddBugaiewController.h"
+
+@interface AddBugaiewController ()
+
+@end
+
+@implementation AddBugaiewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+     self.view.backgroundColor = [UIColor blueColor];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
